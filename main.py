ef754a8d866f52f60457936dacfdd38ff9ef2bac
Diamond Tusks <90947391+DiamondTusks@users.noreply.github.com>
2022-09-22 12:54:24 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -13,15 +13,18 @@ driver = webdriver.Chrome(service=service)
 driver.get("https://www.tinder.com/")
 
 time.sleep(2)
-cookie_accept_button = driver.find_element(By.XPATH, '//*[@id="o-98920890"]/div/div[2]/div/div/div[1]/div[1]/button')
+# cookie_accept_button = driver.find_element(By.XPATH, '//*[@id="o-98920890"]/div/div[2]/div/div/div[1]/div[1]/button')
+cookie_accept_button = driver.find_element(By.XPATH, '//*[@id="s1362659109"]/div/div[2]/div/div/div[1]/div[1]/button')
 cookie_accept_button.click()
 
 time.sleep(2)
-login_button = driver.find_element(By.XPATH, '//*[@id="o-98920890"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+# login_button = driver.find_element(By.XPATH, '//*[@id="o-98920890"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+login_button = driver.find_element(By.XPATH, '//*[@id="s1362659109"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
 login_button.click()
 
 time.sleep(2)
-fb_login_button = driver.find_element(By.XPATH, '//*[@id="o-1827301966"]/main/div/div[1]/div/div/div[3]/span/div[2]/button')
+# fb_login_button = driver.find_element(By.XPATH, '//*[@id="o-1827301966"]/main/div/div[1]/div/div/div[3]/span/div[2]/button')
+fb_login_button = driver.find_element(By.XPATH, '//*[@id="s-365721967"]/main/div/div[1]/div/div/div[3]/span/div[2]/button')
 fb_login_button.click()
 
 base_window = driver.window_handles[0]
@@ -40,7 +43,8 @@ driver.switch_to.window(base_window)
 print(driver.title)
 
 time.sleep(5)
-allow_location = driver.find_element(By.XPATH, '//*[@id="o-1827301966"]/main/div/div/div/div[3]/button[1]')
+# allow_location = driver.find_element(By.XPATH, '//*[@id="o-1827301966"]/main/div/div/div/div[3]/button[1]')
+allow_location = driver.find_element(By.CLASS_NAME, '.onboarding__modal button')
 allow_location.click()
 
 time.sleep(2)
